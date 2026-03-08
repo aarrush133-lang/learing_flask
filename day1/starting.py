b99583192d99179d248a8cd0fe952ce9b750fa31
aarrush133-lang <aarrush133@gmail.com>
2026-03-08 14:49:47 +0530

    add file in static

--- day1/starting.py
+++ day1/starting.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 
-
+name = "aarush"
 app = Flask(__name__)
 
 
@@ -8,7 +8,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return render_template("index.html",name2 = name)
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+
+@app.route("/boot")
+def boot():
+    return render_template("boot.html")
 
 
 app.run()
