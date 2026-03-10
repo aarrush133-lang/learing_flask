efd6a8ef104b7a6f667f020a6e34e6c691fdbb78
aarrush133-lang <aarrush133@gmail.com>
2026-03-10 21:28:10 +0530

    a lot lot

---
+++ day1/main.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+@app.route("/post")
+def home():
+    return render_template("post.html")
+
+app.run()
--- day1/starting.py
+++ day1/starting.py
@@ -1,27 +1,27 @@
-from flask import Flask,render_template
+# from flask import Flask,render_template
 
-name = "aarush"
-app = Flask(__name__)
-
-
-
-
-@app.route("/")
-def hello():
-    return render_template("index.html",name2 = name)
-
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
+# name = "aarush"
+# app = Flask(__name__)
 
 
 
-@app.route("/boot")
-def boot():
-    return render_template("boot.html")
+
+# @app.route("/")
+# def hello():
+#     return render_template("index.html",name2 = name)
 
 
-app.run()
+# @app.route("/home")
+# def home():
+#     return render_template("home.html")
+
+
+
+# @app.route("/boot")
+# def boot():
+#     return render_template("boot.html")
+
+
+# app.run()
 
 
